07d5d6a152f7c1e3e63a8c4ef4a055037d5b308e
YKamenev322 <kamenev.yar@gmail.com>
2021-03-26 20:17:07 +0300

    Joystick constants added

--- Drivers/joystick.cpp
+++ Drivers/joystick.cpp
@@ -13,10 +13,10 @@ axial u - глубина
 */
 
 const Joystick::control_axis Joystick::axis_table[] = {
-    {sf::Joystick::R, 1, SET_YAW},
-    {sf::Joystick::Y, 1, SET_MARCH},
-    {sf::Joystick::X, 1, SET_LAG},
-    {sf::Joystick::U, 1, SET_DEPTH}
+    {sf::Joystick::R, 320, SET_YAW},
+    {sf::Joystick::Y, 320, SET_MARCH},
+    {sf::Joystick::X, 320, SET_LAG},
+    {sf::Joystick::U, 320, SET_DEPTH}
 };
 
 const Joystick::control_buttons Joystick::buttons_table[] = {
