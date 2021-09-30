d300b7181eb3555439826d03d60022961dba96b6
Kuzishchin D.S <kuzds321@gmail.com>
2021-09-30 04:33:10 +0300

    Настройка работы с Git репозиторием: Созданы функции обработки данных репозитория при начале работы и отображение файлов в виде кнопок. Настроена работа команды git pull из основной ui. Подкорректирована основная ui для понимания процессов.

--- SettingsWindow/SetConfiguration/setconfiguration.cpp
+++ SettingsWindow/SetConfiguration/setconfiguration.cpp
@@ -4,6 +4,9 @@
 #include <QDir>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QDebug>
+#include <QPushButton>
+
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 setConfiguration::setConfiguration(QWidget *parent) :
     QWidget(parent),
@@ -14,8 +17,11 @@ setConfiguration::setConfiguration(QWidget *parent) :
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 setConfiguration::~setConfiguration()
 {
-    delete ui;
+    delete vbox_ThrusterSet;
+    delete vbox_VehicleSetting;
+    delete vbox_Devices;
     delete message;
+    delete ui;
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 void setConfiguration::on_pushButton_ThrusterSet_commit_clicked()
@@ -38,6 +44,7 @@ void setConfiguration::on_pushButton_ThrusterSet_commit_clicked()
         message = new DialogCommit;
         message->show();
     }
+
 
 
 }
@@ -85,7 +92,6 @@ void setConfiguration::on_pushButton_devices_commit_clicked()
         message->show();
     }
 
-
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
 void setConfiguration::on_pushButton_pull_clicked()
@@ -95,6 +101,22 @@ void setConfiguration::on_pushButton_pull_clicked()
         configGitBush();
     }
 
+    QProcess* process1 = new QProcess(this);
+    process1->start(filePath, {"-c", "cd " + dirConfig_Path + "\n" + "git pull"});
+    process1->waitForFinished();
+
+    //обновление данных в связи с изменением локального репозитория
+    //---------------------------------------------------------------
+    QDir dir;
+    dir.setPath(dirConfig_Path);
+    dir.setFilter(QDir::AllDirs | QDir::NoDot | QDir::NoDotAndDotDot);
+    QFileInfoList filesList = dir.entryInfoList();
+
+    for(short i = 0; i < (int)filesList.size(); i++)
+    {
+        setElement_GroupBox(filesList[i]);
+    }
+    //---------------------------------------------------------------
 
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -126,13 +148,117 @@ void setConfiguration::configGitBush()
 
         if(fileName == "git-bash.exe")
         {
-            QProcess* process = new QProcess(this);
-            process->start(filePath, {"-c", "'ls'"});
-            process->waitForFinished();
+
             file_flag = true;
+
+            QDir dir;
+            int _dirExists = dir.exists( "SettingsWindow/Config" );
+
+            if( !_dirExists ) // if not, create it
+                dir.mkpath( "SettingsWindow/Config" );
+            dir.setPath("SettingsWindow/Config");
+            dirConfig_Path = dir.absolutePath();
+
+            QProcess* process = new QProcess(this);
+            process->start(filePath, {"-c", "git clone https://github.com/hidronautics/PoolsideGUI_config.git " + dirConfig_Path});
+
+            dir.setFilter(QDir::AllDirs | QDir::NoDot | QDir::NoDotAndDotDot);
+            QFileInfoList filesList = dir.entryInfoList();
+
+            for(short i = 0; i < (int)filesList.size(); i++)
+            {
+                setElement_GroupBox(filesList[i]);
+            }
+
         }
         else
             QMessageBox::information(this, "Error","Select git-bash.exe file");
     }
     else QMessageBox::information(this, "Error","Select second .exe file");
 }
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+void setConfiguration::setElement_GroupBox(QFileInfo file)
+{
+    QString file_path = dirConfig_Path + "/" + file.fileName();
+
+    QDir fileDir(file_path);
+    fileDir.setFilter(QDir::Files | QDir::NoDot | QDir::NoDotAndDotDot);
+    QFileInfoList filesList = fileDir.entryInfoList();
+    QString file_name = file.fileName();
+
+    if(file.fileName() == "ThrusterSet")
+    {
+        if(vbox_ThrusterSet == nullptr)
+        {
+            vbox_ThrusterSet = new QVBoxLayout;
+        }
+        else
+        {
+            delete vbox_ThrusterSet;
+            vbox_ThrusterSet = new QVBoxLayout;
+            qDeleteAll(ui->groupBox_ThrusterSet->findChildren<QWidget*>());
+            ui->groupBox_ThrusterSet->setLayout(vbox_ThrusterSet);
+        }
+
+        for(int i = 0; i < (int)filesList.size(); i++)
+        {
+            QPushButton* button = new QPushButton();
+            button->setText(filesList[i].fileName());
+            vbox_ThrusterSet->addWidget(button);
+        }
+        vbox_ThrusterSet->addStretch(1);
+        ui->groupBox_ThrusterSet->setLayout(vbox_ThrusterSet);
+    }
+
+    if(file.fileName() == "VehicleSetting")
+    {
+        if(vbox_VehicleSetting == nullptr)
+        {
+            vbox_VehicleSetting = new QVBoxLayout;
+        }
+        else
+        {
+            delete vbox_VehicleSetting;
+            vbox_VehicleSetting = new QVBoxLayout;
+            qDeleteAll(ui->groupBox_VehicleSetting->findChildren<QWidget*>());
+            ui->groupBox_VehicleSetting->setLayout(vbox_VehicleSetting);
+        }
+
+        for(int i = 0; i < (int)filesList.size(); i++)
+        {
+            QPushButton* button = new QPushButton();
+            button->setText(filesList[i].fileName());
+            vbox_VehicleSetting->addWidget(button);
+        }
+        vbox_VehicleSetting->addStretch(1);
+        ui->groupBox_VehicleSetting->setLayout(vbox_VehicleSetting);
+    }
+
+    if(file.fileName() == "Devices")
+    {
+        if(vbox_Devices == nullptr)
+        {
+            vbox_Devices = new QVBoxLayout;
+        }
+        else
+        {
+            delete vbox_Devices;
+            vbox_Devices = new QVBoxLayout;
+            qDeleteAll(ui->groupBox_devices->findChildren<QWidget*>());
+            ui->groupBox_devices->setLayout(vbox_Devices);
+        }
+
+        for(int i = 0; i < (int)filesList.size(); i++)
+        {
+            QPushButton* button = new QPushButton();
+            button->setText(filesList[i].fileName());
+            vbox_Devices->addWidget(button);
+        }
+        vbox_Devices->addStretch(1);
+        ui->groupBox_devices->setLayout(vbox_Devices);
+    }
+
+}
+
+
+
--- SettingsWindow/SetConfiguration/setconfiguration.h
+++ SettingsWindow/SetConfiguration/setconfiguration.h
@@ -2,6 +2,8 @@
 #define SETCONFIGURATION_H
 
 #include <QWidget>
+#include <QFileInfo>
+#include <QVBoxLayout>
 #include "dialogcommit.h"
 
 namespace Ui {
@@ -17,11 +19,18 @@ public:
     ~setConfiguration();
 
     void configGitBush(); //функция настройки гит клиента на пк
+    void setElement_GroupBox(QFileInfo); //функция настройки отображения элементов из соответствующих папок репозитория
 
     bool file_flag = false; //переменная обработка .exe приложения
+
     QString filePath; //путь к действующему git-bash.exe
-    QString fileName; //имя файла к которому указывает путь
+    QString fileName; //имя файла к которому указывает путь к действующему git-bash.exe
+    QString dirConfig_Path; //путь к директории конфигураций
+
     DialogCommit* message = nullptr;
+    QVBoxLayout* vbox_ThrusterSet = nullptr;
+    QVBoxLayout* vbox_VehicleSetting = nullptr;
+    QVBoxLayout* vbox_Devices = nullptr;
 
 private slots:
     void on_pushButton_ThrusterSet_commit_clicked();
